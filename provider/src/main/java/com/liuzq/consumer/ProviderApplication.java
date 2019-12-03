bf3915a2a3355e061234155f7d6e023aa0b7cb28
liuzequan <liuzequan@lemoncome.com>
2019-12-03 17:50:11 +0800

    zipkin-内容提交

--- provider/src/main/java/com/liuzq/consumer/ProviderApplication.java
+++ provider/src/main/java/com/liuzq/consumer/ProviderApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ConsumerApplication {
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
 }
